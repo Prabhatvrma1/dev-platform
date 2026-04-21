bb67212e0f8c546ae071e908b57c508584e945e3
Prabhat Verma <prabhatsoni0109@gmail.com>
2026-04-21 17:00:12 +0530

    updated file app.js

--- src/app.js
+++ src/app.js
@@ -3,13 +3,14 @@ const http = require('http');
 
 const app = react();
 
-app.get('/', (req,res) =>{
-    res.send('Hello World');
-}); 
 
 app.get('/hello', (req, res) =>{
      res.send("hello helo");
 })
+
+app.get('/', (req,res) =>{
+    res.send('Hello World');
+}); 
 
 app.listen(3000, ( req, res) =>{
     console.log('Server is running on port 3000');
