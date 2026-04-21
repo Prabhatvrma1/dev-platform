bda87ce04fbce12d0d164687bc40ee28dec44e09
Prabhat Verma <prabhatsoni0109@gmail.com>
2026-04-21 17:29:30 +0530

    understanding routes

--- src/app.js
+++ src/app.js
@@ -4,14 +4,51 @@ const http = require('http');
 const app = react();
 
 
-app.get('/hello', (req, res) =>{
-     res.send("hello helo");
+// if you use app.use / user here igt will then just use this one and all the api  will get uselesss
+
+
+app.get('/user', (req, res) =>{
+    res.send({
+        name: "hi",
+        city:"jojo"
+    })
+});
+
+app.post( "/user", (req, res) => {
+    console.log("data base saved");
+    res.send("data sucessfully saved");
+
+});
+
+app.delete('/user', (req, res) => {
+    res.send("datat delted sucessfully");
 })
 
-app.get('/', (req,res) =>{
-    res.send('Hello World');
-}); 
 
 app.listen(3000, ( req, res) =>{
     console.log('Server is running on port 3000');
 });
+
+
+
+
+
+
+// //https methods - post , get ,patch , delete
+
+// app.get('/hello', (req, res) =>{
+//      res.send("hello helo");
+// })
+
+
+// //get api call when like we dp /xyz 
+
+// app.get('/', (req,res) =>{
+//     res.send('Hello World');
+// }); 
+
+
+// app.use('/test', (req, res) =>{
+//     res.send("hello helo");
+// });
+// //this will match all the api req to /test so we can get data in both get and post in using use but when we are usining the get and post it will react to the called one
